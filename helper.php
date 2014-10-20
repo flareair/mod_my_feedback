097771c230619a0526c06c1bbf7ce6e8dfcc27b4
flareair <flareair0@gmail.com>
2014-10-20 18:02:53 +0400

    Solved json transition problem

--- helper.php
+++ helper.php
@@ -7,9 +7,10 @@ class ModMyFeedbackHelper
   public static function getAjax()
   {
     $input = JFactory::getApplication()->input;
-    $firstname  = $input->get('firstname');
-    $lastname  = $input->get('lastname');
-    $data = $input->get('data');
-    return  json_encode($data);
+    $firstname  = $input->getString('firstname');
+    $lastname  = $input->getString('lastname');
+    $data = $input->getString('data');
+    $formData = json_decode($data);
+    return  $formData;
   }
 }
